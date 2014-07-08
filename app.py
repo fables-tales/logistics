606db5135bfb1ffb0ff5a240a87d525c1771eb3e
Sam Phippen <samphippen@googlemail.com>
2014-07-07 19:25:58 -0500

    User facing copy to file and alert colors

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
 import tempfile
+import json
 import uuid
 
 from shutil import move
@@ -8,9 +9,11 @@ from shutil import move
 from flask import Flask, render_template, request, session, flash, redirect
 
 app = Flask(__name__)
-
 def base_path():
     return os.path.dirname(os.path.abspath(__file__))
+
+def copy_for(event):
+    return json.loads(open(os.path.join(base_path(), "strings.json")).read())[event]
 
 
 @app.route("/")
@@ -27,7 +30,19 @@ def export_project():
     dir_to_work_in = tempfile.mkdtemp()
     user_code_dir = os.path.join(dir_to_work_in, "user_code")
 
-    subprocess.check_call(["git", "clone", git_url, user_code_dir])
+    wanted_return_type = request.accept_mimetypes.best_match(["application/json", "text/html", "text/plain"])
+
+    try:
+        subprocess.check_call(["git", "clone", git_url, user_code_dir])
+    except Exception as e:
+        if wanted_return_type == "text/html":
+            flash(copy_for("EXPORT_FAIL"), "failure")
+            return redirect("/")
+        elif wanted_return_type == "text/json":
+            return json.dumps({"errors": ["EXPORT_FAIL"]}), 400
+        else:
+            return "EXPORT_FAIL", 400
+
 
     zip_path = os.path.join(dir_to_work_in, "robot.zip")
 
@@ -43,7 +58,7 @@ def export_project():
 
     wanted_return_type = request.accept_mimetypes.best_match(["application/json", "text/html", "text/plain"])
     if wanted_return_type == "text/html":
-        flash("Repository exported successfully, prompting to download")
+        flash(copy_for("EXPORT_SUCCESS"), "success")
         session["zip_url"] = "/%s" % (zip_url,)
         return redirect("/")
     elif wanted_type == "text/json":
